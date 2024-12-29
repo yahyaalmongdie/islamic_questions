ae3b8661f1e3f5c34432babd532f9a1dee132b93
yahya almongdie <yahyaalmongdie@gmail.com>
2024-12-29 20:40:38 +0300

    create app colors

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color blackColor = Color(0xff202020);
+  static const Color goldColor = Color(0xffE2BE7F);
+  static const Color grayColor = Color(0xff707070);
+  static const Color whiteColor = Color(0xffffffff);
+}
