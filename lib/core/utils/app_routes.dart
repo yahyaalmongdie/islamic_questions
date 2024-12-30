c2a3a0db75e9e4008e9326f79e3a15fd5758e67e
yahya almongdie <yahyaalmongdie@gmail.com>
2024-12-30 21:02:48 +0300

    create levels view

--- lib/core/utils/app_routes.dart
+++ lib/core/utils/app_routes.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:islamic_questions/features/home/presentation/view/pages/home_view.dart';
+import 'package:islamic_questions/features/questions/presentation/view/pages/levels_view.dart';
 import 'package:islamic_questions/features/splash/presentation/view/pages/spalsh_view.dart';
 
 abstract class AppRoutes {
   static const String kHomeView = "/homeView";
+  static const String kLevelsiew = "/levelsView";
 
   static final GoRouter router = GoRouter(routes: <RouteBase>[
     GoRoute(
@@ -18,7 +20,13 @@ abstract class AppRoutes {
           builder: (BuildContext context, GoRouterState state) {
             return const HomeView();
           },
-        )
+        ),
+        GoRoute(
+          path: "levelsView",
+          builder: (BuildContext context, GoRouterState state) {
+            return const LevelsView();
+          },
+        ),
       ],
     ),
   ]);
---
+++ lib/features/questions/presentation/view/pages/levels_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LevelsView extends StatelessWidget {
+  const LevelsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
