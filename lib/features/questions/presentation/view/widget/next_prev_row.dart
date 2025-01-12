8942885e299ad2670c552ffc31e398a9d5a65505
yahya almongdie <yahyaalmongdie@gmail.com>
2025-01-12 20:40:13 +0300

    create show score button

--- lib/features/questions/presentation/view/widget/next_prev_row.dart
+++ lib/features/questions/presentation/view/widget/next_prev_row.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:islamic_questions/core/utils/app_assets.dart';
 import 'package:islamic_questions/core/utils/app_colors.dart';
 import 'package:islamic_questions/core/utils/app_text_styles.dart';
-import 'package:islamic_questions/features/questions/presentation/manager/options_cubit/options_cubit.dart';
 import 'package:islamic_questions/features/questions/presentation/manager/questions_cubit/questions_cubit.dart';
 import 'package:islamic_questions/features/questions/presentation/view/widget/custom_arrow_widget.dart';
-import 'package:islamic_questions/features/questions/presentation/view/widget/score_dailog.dart';
+import 'package:islamic_questions/features/questions/presentation/view/widget/show_score_button.dart';
 
 class NextPrevRow extends StatelessWidget {
   const NextPrevRow({super.key, required this.questionsList});
@@ -97,51 +95,6 @@ class NextPrevRow extends StatelessWidget {
           ),
         ],
       ),
-    );
-  }
-}
-
-class ShowScoreButton extends StatelessWidget {
-  const ShowScoreButton({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocConsumer<QuestionsCubit, QuestionsState>(
-      listener: (context, state) {},
-      builder: (context, state) {
-        return Visibility(
-          visible: BlocProvider.of<QuestionsCubit>(context).showScoreButton,
-          child: CustomArrowWidget(
-            onPressed: () {
-              var cubit = BlocProvider.of<OptionsCubit>(context);
-              if (cubit.score > 8) {
-                return scoreDailog(
-                    context: context,
-                    score: cubit.score,
-                    image: Assets.imagesScore910);
-              } else if (cubit.score <= 8 && cubit.score > 6) {
-                return scoreDailog(
-                    context: context,
-                    score: cubit.score,
-                    image: Assets.imagesScore78);
-              } else if (cubit.score <= 6 && cubit.score >= 5) {
-                return scoreDailog(
-                    context: context,
-                    score: cubit.score,
-                    image: Assets.imagesScore56);
-              } else {
-                return scoreDailog(
-                    context: context,
-                    score: cubit.score,
-                    image: Assets.imagesScore14);
-              }
-            },
-            iconData: Icons.arrow_forward_ios,
-          ),
-        );
-      },
     );
   }
 }
