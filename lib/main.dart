4e8c967cbe6a6ad56960026270aabc8d925d8660
yahya almongdie <yahyaalmongdie@gmail.com>
2025-01-13 18:33:12 +0300

    cofigration admob ads

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:islamic_questions/core/utils/app_colors.dart';
 import 'package:islamic_questions/core/utils/app_routes.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await MobileAds.instance.initialize();
   runApp(const MyApp());
 }
 
